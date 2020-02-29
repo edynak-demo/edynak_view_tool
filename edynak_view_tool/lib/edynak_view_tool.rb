a1510c2d69546640d2c4b1c2159e9ed647653333
edynak-demo <edynakdemo@gmail.com>
2020-02-29 16:05:13 -0500

    Edynak view tool initial commit

---
+++ edynak_view_tool/lib/edynak_view_tool.rb
@@ -0,0 +1,7 @@
+require "edynak_view_tool/version"
+require "edynak_view_tool/renderer"
+
+module EdynakViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ edynak_view_tool/lib/edynak_view_tool/version.rb
@@ -0,0 +1,3 @@
+module EdynakViewTool
+  VERSION = "0.1.0"
+end
